f2a5d6406fc8b0c70e5c5f8eda82f80492235d6e
Natasha <natasha1434@gmail.com>
2024-11-21 17:13:07 -0500

    Initial tests written, 200 successful

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,25 @@
-use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, post, App, HttpResponse, HttpServer};
 use actix_web::dev::Server;
 use std::net::TcpListener;
 
 #[get("/health_check")]
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok()
+async fn health_check() -> HttpResponse {
+    HttpResponse::Ok().finish()
 }
 
+#[post("/subscriptions")]
+async fn subscriptions() -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
 
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| { 
-            App::new()
-                .service(health_check)
-        })
-        .listen(listener)?
-        .run();
+        App::new()
+            .service(health_check)
+            .service(subscriptions)
+    })
+    .listen(listener)?
+    .run();
     Ok(server)
 }
 
